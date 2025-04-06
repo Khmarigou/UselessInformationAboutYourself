56d3eb04e19100f59e0648e0fac504b3d33cff08
Khmarigou <luca.plc@outlook.fr>
2025-04-06 22:29:01 +0200

    Changing application theme

---
+++ app/src/main/java/com/example/uselessinformationaboutyourself/views/infoViews/HeightComparaisons.kt
@@ -0,0 +1,24 @@
+package com.example.uselessinformationaboutyourself.views.infoViews
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun HeightComparaisons(height: Int, modifier: Modifier = Modifier) {
+    val heightInM = height / 100.0
+    val heightEiffelTower = 330 // in meters
+    val heightStatueOfLiberty = 93 // in meters
+    val heightEmpireStateBuilding = 443 // in meters
+    val heightBurjKhalifa = 828 // in meters
+
+    Column (
+        modifier = modifier
+    ) {
+        Text("On peut vous placer ${"%.2f".format(heightEiffelTower / heightInM)} fois dans la Tour Eiffel")
+        Text("On peut vous placer ${"%.2f".format(heightStatueOfLiberty / heightInM)} fois dans la Statue de la Liberté")
+        Text("On peut vous placer ${"%.2f".format(heightEmpireStateBuilding / heightInM)} fois dans l'Empire State Building")
+        Text("On peut vous placer ${"%.2f".format(heightBurjKhalifa / heightInM)} fois dans le Burj Khalifa")
+    }
+}
